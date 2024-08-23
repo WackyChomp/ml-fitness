ec7cbc99ffebbd0b81094625d9d703a20a7d8f33
Wilson Chan <wilsonchan54@gmail.com>
2024-08-22 23:08:15 -0400

    Added comments and extractedstrings from filename in make_dataset.py / pip installed pandas and updated requirements.txt

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,31 @@
+import pandas as pd
+from glob import glob
 
 # --------------------------------------------------------------
 # Read single CSV file
 
+single_file_1 = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv')
+single_file_2 = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv')
 
 # --------------------------------------------------------------
 # List all data from ./data/raw
+files = glob("../../data/raw/MetaMotion/*.csv")   # gets all files from directory
+len(files)
+
+# --------------------------------------------------------------
+# Extract strings from filename and append to dataframe
+    # (participant / label / category)
+
+# see splits between '-' with --- f.split('-')
+
+data_path = '../../data/raw/MetaMotion/'
+f = files[0]    # 1st dataset of the directory
+
+f.split('-')      # splitting the filename into list
+
+participant = f.split('-')[0].replace(data_path, "")
+label = f.split('-')[1]
+category = f.split('-')[2].strip('123')       # removes (1-3) numbers that appear at the end of a category
 
 
 # --------------------------------------------------------------
