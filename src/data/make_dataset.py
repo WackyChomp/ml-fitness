393c459c5065a07c55b08cdac8c4b02fc5eac5fc
Wilson Chan <wilsonchan54@gmail.com>
2024-08-26 23:43:25 -0400

    Created function that reads files and manipulates dataset by combining code snippets within make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -84,6 +84,53 @@ del gyr_df['elapsed (s)']
 
 
 # --------------------------------------------------------------
+# Combine above 
+files = glob("../../data/raw/MetaMotion/*.csv")   # gets all files from directory
+
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files:
+        data_path = '../../data/raw/MetaMotion'
+        participant = f.split('-')[0].replace(data_path, "").replace('\\', "")          # remove "\\" from the path
+        label = f.split('-')[1]
+        category = f.split('-')[2].strip('123').strip('_MetaWear_2019')       # removes (1-3) numbers that appear at the end of a category
+
+        df = pd.read_csv(f)
+
+        df['participant'] = participant
+        df['label'] = label
+        df['category'] = category
+
+        if "Accelerometer":
+            df['set'] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+        if "Gyroscope" in f:
+            df['set'] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
+    gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit='ms')
+
+
+    del acc_df['epoch (ms)']
+    del acc_df['time (01:00)']
+    del acc_df['elapsed (s)']
+
+    del gyr_df['epoch (ms)']
+    del gyr_df['time (01:00)']
+    del gyr_df['elapsed (s)']
+
+    return acc_df, gyr_df
+
+acc_df, gyr_df = read_data_from_files(files)
+
+# --------------------------------------------------------------
 # Merging datasets
 
 
