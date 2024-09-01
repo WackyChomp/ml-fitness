f610663f15e5dd4d3c8be3add7f64fbf501bdeaa
Wilson Chan <wilsonchan54@gmail.com>
2024-08-31 23:09:36 -0400

    Created for loop to display each unique exercises in visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from IPython.display import display     # display dataframes within a loop
 
 # --------------------------------------------------------------
 # Load data
@@ -15,9 +16,26 @@ set_df = df[df['set'] == 1]
 plt.plot(set_df['acc_y'])
 plt.plot(df['acc_y'])
 
+plt.plot(set_df['acc_y'].reset_index(drop=True))      # reset index to show number of samples
+
 # --------------------------------------------------------------
 # Plot all exercise types
 
+#df['label'].unique()
+
+
+# Displays all the existing samples
+for label in df['label'].unique():
+  subset = df[df['label'] == label]
+  #display(subset.head())       # display dataframe within a loop
+
+  fig, ax = plt.subplots()
+
+  plt.plot(subset['acc_y'].reset_index(drop=True), label=label)             # displays all samples
+  #plt.plot(subset[:100]['acc_y'].reset_index(drop=True), label=label)      # displays first 100 samples
+
+  plt.legend()
+  plt.show()
 
 
 # --------------------------------------------------------------
