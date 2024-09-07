51fc981aab2dc7af567615f7daf3b8551d7d866c
Wilson Chan <wilsonchan54@gmail.com>
2024-09-06 21:43:26 -0400

    Added title to figure that combined single lable and participant / created a for loop that combined all labels and participants (able to save individual figures)

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -148,6 +148,34 @@ fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
 combined_plot_df[['acc_x', 'acc_y', 'acc_z']].plot(ax=ax[0])
 combined_plot_df[['gyr_x', 'gyr_y', 'gyr_z']].plot(ax=ax[1])
 
+plt.title(f'{label} ({participant})'.title())
 ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
 ax[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
 ax[1].set_xlabel('samples')
+plt.show()
+# --------------------------------------------------------------
+# Loop through all combinations of "labels" and "participants"
+
+labels = df['label'].unique()
+participants = df['participant'].unique()
+
+for label in labels:
+  for participant in participants:
+    all_combinations_plot_df = (
+      df.query(f'label == "{label}"')
+      .query(f'participant == "{participant}"')
+      .reset_index()
+    )
+
+    if len(all_combinations_plot_df) > 0:
+      fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+      all_combinations_plot_df[['acc_x', 'acc_y', 'acc_z']].plot(ax=ax[0])
+      all_combinations_plot_df[['gyr_x', 'gyr_y', 'gyr_z']].plot(ax=ax[1])
+
+      plt.title(f'{label} ({participant})'.title())
+      ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+      ax[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3, fancybox=True, shadow=True)
+      ax[1].set_xlabel('samples')
+
+      #plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+      plt.show()
